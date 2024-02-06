5377908ca6cbdd3db9f7aa84ea2cdf45a7678827
[Yann Baraguay] <[yann.baraguay@gmail.com]>
2024-02-06 15:08:03 +0100

    creation du models coiffeurs

--- models/coiffeur.go
+++ models/coiffeur.go
@@ -1,8 +1,13 @@
 package models
 
-type coiffeur struct{
-
-	Name string `json:"name"`
+type coiffeur struct {
+	Name     string `json:"name"`
 	Lastname string `json:"lastname"`
-	Salon string `json:"salon"`
+	Salon    string `json:"salon"`
+}
+
+type coiffeurStoreInterface interface {
+	GetCoiffeur() ([]Salon, error)
+	AddCoiffeur(item Salon) (int, error)
+	DeleteCoiffeur(id int) error
 }
